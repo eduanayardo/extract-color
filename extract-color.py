9a14e589ec573e02e9d708e9a86af44c709bb77f
Eduardo Altamirano Anaya <eduanayardo@gmail.com>
2023-10-11 12:31:56 -0700

    Update code to extract hexadecimal, rgb and cmyk values

--- extract-color.py
+++ extract-color.py
@@ -1,39 +1,65 @@
 from PIL import Image
-img = Image.open("1.jpg")
-
-import numpy #as np
+import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
-image = mpimg.imread('1.jpg')
-w, h, d = tuple (image.shape)
-pixels = numpy.reshape(image, (w * h, d))
-
 from sklearn.cluster import KMeans
 
-n_colors = 10 # change this to get more or fewer colors
-model = KMeans(n_clusters=n_colors, random_state=42).fit (pixels)
-palette = numpy.uint8(model.cluster_centers_)
+# Function to calculate CMYK values from RGB values
+# Función para calcular los valores CMYK a partir de los valores RGB
+def rgb_to_cmyk(rgb):
+    r, g, b = [x / 255.0 for x in rgb]
+    k = 1 - max(r, g, b)
+    c = (1 - r - k) / (1 - k) if (1 - k) != 0 else 0
+    m = (1 - g - k) / (1 - k) if (1 - k) != 0 else 0
+    y = (1 - b - k) / (1 - k) if (1 - k) != 0 else 0
+    return (round(c, 2), round(m, 2), round(y, 2), round(k, 2))
 
+# Load the image (Cargar la imagen)
+img = Image.open("3.jpg")
+image = np.array(img)
 
-def rgb_to_hex(r, g, b):
-     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+# Get the dimensions of the image (Obtener las dimensiones de la imagen)
+w, h, d = image.shape
 
-colors = []
-for i in palette:
-    ind = 0
-    rgb  = ""
-    for e in i:
-        if ind == 2:
-            rgb = rgb + str(e)
-            rgb1 = rgb.split(",")
-            colors.append(rgb_to_hex(int(rgb1[0]),int(rgb1[1]),int(rgb1[2])))
-            rgb = ""
-        else:
-            rgb = rgb + str(e) + ","
-        ind = ind + 1
-print(colors)
+# Flatten the pixel matrix (Aplanar la matriz de píxeles)
+pixels = image.reshape((w * h, d))
 
-img.show()
-plt.imshow([palette])
+# Number of colors to extract (Número de colores a extraer)
+n_colors = 10
+
+# Fit the KMeans model (Ajustar el modelo KMeans)
+model = KMeans(n_clusters=n_colors, random_state=42).fit(pixels)
+
+# Get the colors in the palette in hexadecimal and RGB format (Obtener los colores de la paleta en formato hexadecimal y RGB)
+palette_hex = ['#%02x%02x%02x' % tuple(map(int, color)) for color in model.cluster_centers_]
+palette_rgb = [tuple(map(int, color)) for color in model.cluster_centers_]
+
+# Calculate CMYK values (Calcular los valores CMYK)
+palette_cmyk = [rgb_to_cmyk(rgb) for rgb in palette_rgb]
+
+# Display the original image, the color palette, and color information in one panel (Mostrar la imagen original, la paleta de colores y la información de colores en un solo panel)
+plt.figure(figsize=(15, 6))
+
+# Panel 1: Original Image (Panel 1: Imagen Original)
+plt.subplot(1, 3, 3)
+plt.title("Imagen Original")
+plt.axis('off')
+plt.imshow(image)
+
+# Panel 2: Color Palette (Panel 2: Paleta de Colores)
+plt.subplot(1, 3, 2)
+plt.title("Paleta de Colores")
+plt.axis('off')
+color_palette = np.array([palette_rgb], dtype=np.uint8)
+plt.imshow(color_palette)
+
+# Panel 3: Color Information (RGB, Hex, CMYK) (Panel 3: Información de Colores (RGB, Hexadecimal, CMYK))
+plt.subplot(1, 3, 1)
+plt.title("Información de Colores", y=0.9)
+plt.axis('off')
+color_info = ""
+for i, (hex_color, rgb_color, cmyk_color) in enumerate(zip(palette_hex, palette_rgb, palette_cmyk)):
+    color_info += f"Color {i+1}: Hex: {hex_color} - RGB: {rgb_color} - CMYK: {cmyk_color}\n"
+plt.text(0.5 * (.25 + 0.5), 0.5 * (.25 + .5), color_info, ha='center', va='center', fontsize=10)
+
+plt.tight_layout()
 plt.show()
