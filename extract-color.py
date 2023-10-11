2dc9b8c6d4908c7b0316aeed16811782c342023e
Eduardo Altamirano Anaya <eduanayardo@gmail.com>
2023-10-10 22:41:31 -0700

    Extraction hexadecimal colors

--- extract-color.py
+++ extract-color.py
@@ -15,5 +15,23 @@ n_colors = 10
 model = KMeans(n_clusters=n_colors, random_state=42).fit (pixels)
 palette = numpy.uint8(model.cluster_centers_)
 
+def rgb_to_hex(r, g, b):
+     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+colors = []
+for i in palette:
+    ind = 0
+    rgb  = ""
+    for e in i:
+        if ind == 2:
+            rgb = rgb + str(e)
+            rgb1 = rgb.split(",")
+            colors.append(rgb_to_hex(int(rgb1[0]),int(rgb1[1]),int(rgb1[2])))
+            rgb = ""
+        else:
+            rgb = rgb + str(e) + ","
+        ind = ind + 1
+print(colors)
+
 plt.imshow([palette])
 plt.show()
